61560dd11a45f5fa43c164d58983c63015c89584
MNihal7961 <mnihal7961@gmail.com>
2025-07-23 17:21:03 +0530

    feat: enhance PWA support with manifest and service worker configuration
    - Add VitePWA plugin to Vite configuration for automatic service worker registration
    - Include assets and manifest details for PWA in vite.config.ts
    - Create site.webmanifest file for web app configuration with icons and display settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon/favicon.ico',
+        'favicon/apple-touch-icon.png',
+        'favicon/favicon.svg'
+      ],
+      manifest: {
+        name: 'CopyUI',
+        short_name: 'CopyUI',
+        description: 'CopyUI | Copy.Paste.Build',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/favicon/favicon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon/favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
