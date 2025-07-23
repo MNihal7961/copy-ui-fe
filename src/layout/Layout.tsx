dcca687e74e156bafe1296d0e5dd1fb5d417e26d
MNihal7961 <mnihal7961@gmail.com>
2025-07-23 17:40:35 +0530

    feat: create Layout component to structure application with Header and Footer

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <section className="max-w-screen-xl flex flex-col items-start justify-center mx-auto p-4">
+          {children}
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
