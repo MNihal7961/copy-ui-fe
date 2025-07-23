4d7e67a5a039badc32aa54a5c145100e61193ca6
MNihal7961 <mnihal7961@gmail.com>
2025-07-23 19:01:25 +0530

    feat: enhance Home component with Hero and GetStarted sections
    - Import Hero and GetStarted components for improved user engagement
    - Update Home component layout to use flexbox for better alignment and spacing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-y-6">
+      <Hero />
+      <GetStarted/>
+    </div>
+  );
 };
 
-export default Home; 
+export default Home;
