840fc47491d511d3388229c873e914ed2477f564
MNihal7961 <mnihal7961@gmail.com>
2025-12-30 14:24:10 +0530

    Update Router to include new component routes for Alert, Avatar, Badge, Banner, Bottom Navigation, Breadcrumb, Button, Button Group, Card, Carousel, Chat Bubble, and Clipboard pages.

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -8,6 +8,18 @@ import GettingStarted from "../Pages/GettingStarted/GettingStarted";
 import ComponentPage from "../Pages/ComponentPage/ComponentPage";
 import AccordionPage from "../Pages/AccordionPage/AccordionPage";
 import NotFound from "../Pages/NotFound/NotFound";
+import AlertPage from "../Pages/AlertPage/AlertPage";
+import AvatarPage from "../Pages/AvatarPage/AvatarPage";
+import BadgePage from "../Pages/BadgePage/BadgePage";
+import BannerPage from "../Pages/BannerPage/BannerPage";
+import BottomNavigationPage from "../Pages/BottomNavigationPage/BottomNavigationPage";
+import BreadcrumbPage from "../Pages/BreadcrumbPage/BreadcrumbPage";
+import ButtonPage from "../Pages/ButtonPage/ButtonPage";
+import ButtonGroupPage from "../Pages/ButtonGroupPage/ButtonGroupPage";
+import CardPage from "../Pages/CardPage/CardPage";
+import CarouselPage from "../Pages/CarouselPage/CarouselPage";
+import ChatBubblePage from "../Pages/ChatBubblePage/ChatBubblePage";
+import ClipBoardPage from "../Pages/ClipBoardPage/ClipBoardPage";
 
 const Router: React.FC = () => {
   return (
@@ -22,6 +34,18 @@ const Router: React.FC = () => {
         {/* Components */}
         <Route path="components" element={<ComponentPage />} />
         <Route path="accordion" element={<AccordionPage />} />
+        <Route path="alert" element={<AlertPage />} />
+        <Route path="avatar" element={<AvatarPage />} />
+        <Route path="badge" element={<BadgePage />} />
+        <Route path="banner" element={<BannerPage />} />
+        <Route path="bottom-navigation" element={<BottomNavigationPage />} />
+        <Route path="breadcrumb" element={<BreadcrumbPage />} />
+        <Route path="button" element={<ButtonPage />} />
+        <Route path="button-group" element={<ButtonGroupPage />} />
+        <Route path="card" element={<CardPage />} />
+        <Route path="carousel" element={<CarouselPage />} />
+        <Route path="chat-bubble" element={<ChatBubblePage />} />
+        <Route path="clipboard" element={<ClipBoardPage />} />
 
         {/* Not Found */}
         <Route path="*" element={<NotFound />} />
